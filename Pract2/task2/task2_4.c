9c8e41780a8b6dca2f2907c1320f731e28f5ffad
Tymoshchenko Vladyslav <102964219+timosjr@users.noreply.github.com>
2025-03-27 15:43:19 +0200

    Create task2_4.c

---
+++ Pract2/task2/task2_4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int local_arr1[1000];         // Локальний масив без ініціалізації
+    int local_arr2[1000] = {1};   // Локальний масив із ініціалізацією
+
+    printf("Hello, World!\n");
+    return 0;
+}
