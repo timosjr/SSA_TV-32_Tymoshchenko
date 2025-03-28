7ab58bde1a7d7c04d36a6f8f2023148d7bffd4fc
Tymoshchenko Vladyslav <102964219+timosjr@users.noreply.github.com>
2025-03-28 14:49:46 +0200

    Update task3.c

--- Pract4/task3/task3.c
+++ Pract4/task3/task3.c
@@ -5,17 +5,17 @@ int main() {
     void* ptr0 = malloc(0);
     
     if (ptr0 == NULL) {
-        printf("malloc(0) повернув NULL\n");
+        printf("malloc(0) returned NULL\n");
     } else {
-        printf("malloc(0) повернув вказівник: %p\n", ptr0);
+        printf("malloc(0) returned pointer: %p\n", ptr0);
     }
 
     void* ptr3 = malloc(3);
     
     if (ptr3 == NULL) {
-        printf("malloc(3) повернув NULL\n");
+        printf("malloc(3) returned NULL\n");
     } else {
-        printf("malloc(3) повернув вказівник: %p\n", ptr3);
+        printf("malloc(3) returned pointer: %p\n", ptr3);
     }
 
     free(ptr0);
