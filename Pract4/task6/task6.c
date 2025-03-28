95ee99d8bce883bd7b26c42674567ff424893c5b
Tymoshchenko Vladyslav <102964219+timosjr@users.noreply.github.com>
2025-03-28 14:54:42 +0200

    Update task6.c

--- Pract4/task6/task6.c
+++ Pract4/task6/task6.c
@@ -5,18 +5,18 @@ int main() {
     // Викликаємо realloc з NULL (еквівалентно malloc)
     int *ptr = realloc(NULL, 5 * sizeof(int));
     if (ptr == NULL) {
-        printf("Не вдалося виділити пам'ять.\n");
+        printf("Can not allocate memory\n");
         return 1;
     } else {
-        printf("Пам'ять виділена успішно для 5 елементів.\n");
+        printf("Memory allocated\n");
     }
 
     // Викликаємо realloc з розміром 0 (еквівалентно free)
     int *temp = realloc(ptr, 0);
     if (temp == NULL) {
-        printf("Пам'ять успішно звільнена (temp == NULL).\n");
+        printf("Memory free\n");
     } else {
-        printf("Пам'ять не звільнена, temp != NULL.\n");
+        printf("Memory not free, temp != NULL.\n");
         // За допомогою temp перевірте, чи вона була перепризначена
         free(temp);  // Важливо: звільніть пам'ять після перевірки
     }
