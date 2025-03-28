0ae0a5446e069dc80f08cd1cfd88149b29c68896
Tymoshchenko Vladyslav <102964219+timosjr@users.noreply.github.com>
2025-03-28 14:54:24 +0200

    Update task5.c

--- Pract4/task5/task5.c
+++ Pract4/task5/task5.c
@@ -11,13 +11,13 @@ void example() {
     // Імітуємо ситуацію, де realloc не може виділити пам'ять
     int *new_arr = realloc(arr, 1000000000 * sizeof(int));  // Спробуємо виділити занадто багато пам'яті
     if (new_arr == NULL) {
-        printf("Помилка realloc: не вдалося виділити пам'ять\n");
+        printf("Error realloc: can not allocate memory\n");
         free(arr);  // Звільняємо стару пам'ять
         return;
     }
 
     // Якщо realloc успішний
-    printf("Пам'ять успішно перевиділено\n");
+    printf("Memory allocated\n");
 
     free(new_arr);  // Звільняємо пам'ять
 }
